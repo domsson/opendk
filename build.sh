c420c709a5e1aaedbf85373e5ebf26b4c1f5385e
Julien Dau <julien.dau@student.htw-berlin.de>
2016-08-03 23:15:30 +0200

    Build script was missing some parameters for the recently added line

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd src
 g++ -c *.cpp -std=c++11 -Wall
-g++ -c engine/opengl/*.cpp
+g++ -c engine/opengl/*.cpp -std=c++11 -Wall
 g++ -o ../bin/opendk *.o -lGL -lGLEW  -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio 
 rm ./*.o
 cd ..

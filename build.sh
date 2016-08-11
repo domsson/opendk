df52f80dd9b257f294b324b5063bd704577ba4ec
Julien Dau <julien.dau@student.htw-berlin.de>
2016-08-11 02:01:40 +0200

    Amended the build script so it will compile stuff in the 'utils' directory as well

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cd src
 g++ -c *.cpp -std=c++11 -Wall
 g++ -c engine/opengl/*.cpp -std=c++11 -Wall
-g++ -o ../bin/opendk *.o -lGL -lGLEW  -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio 
+g++ -c utils/*.cpp -std=c++11 -Wall
+g++ -o ../bin/opendk *.o -lGL -lGLEW  -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 rm ./*.o
 cd ..

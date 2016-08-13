4fee05ba763f4c61c25a9f8f7ebb24cbf607a4ae
Julien Dau <julien.dau@student.htw-berlin.de>
2016-08-13 17:15:09 +0200

    build script now also compiles files within src/engine

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd src
 g++ -c *.cpp -std=c++11 -Wall
+g++ -c engine/*.cpp -std=c++11 -Wall
 g++ -c engine/opengl/*.cpp -std=c++11 -Wall
 g++ -c utils/*.cpp -std=c++11 -Wall
 g++ -o ../bin/opendk *.o -lGL -lGLEW  -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio

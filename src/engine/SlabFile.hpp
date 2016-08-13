5047b6e548d8cb00c98123e428f271a95cceac15
Julien Dau <julien.dau@student.htw-berlin.de>
2016-08-13 17:26:23 +0200

    Added comments to all vars and methods of SlabFile

--- src/engine/SlabFile.hpp
+++ src/engine/SlabFile.hpp
@@ -39,6 +39,7 @@ namespace OpenDK
 		/**
 		 * Creates a new SLB file representation by loading
 		 * the given SLB file into memory.
+		 * @param filePath The file path of the SLB file to load
 		 */
 		SlabFile(const std::string& filePath);
 
@@ -48,22 +49,62 @@ namespace OpenDK
 		 */
 		~SlabFile();
 
+		/**
+		 * Load the given SLB file into memory.
+		 * @param filePath The file path of the SLB file to load
+		 */
 		bool load(const std::string& filePath);
+
+		/**
+		 * Load the given non-standard sized SLB file into memory.
+		 * @param filePath The file path of the SLB file to load
+		 * @param mapWidth The width of the map described with this SLB file
+		 * @param mapHeight The height of the map described with this SLB file
+		 */
 		bool load(const std::string& filePath, int mapWidth, int mapHeight);
 
+		/**
+		 * Prints a simple representation of the SLB file (map layout)
+		 * to the console, where every tile is two characters wide and
+		 * one character height.
+		 */
 		void printMap() const;
 
-		int getMapHeight() const;
+		/**
+		 * @return The width of the loaded map layout or -1 if none loaded yet
+		 */
 		int getMapWidth() const;
 
+		/**
+		 * @return The height of the loaded map layout or -1 if none loaded yet
+		 */
+		int getMapHeight() const;
+
+		/**
+		 * @return The byte value (tile type) of the tile at position x,y or -1
+		 */
 		char getTileType(int x, int y) const;
 
 	private:
 
+		/**
+		 * The odd bytes (as the even bytes are all zero) of the loaded SLB file
+		 */
 		char* mapLayout;
+
+		/**
+		 * The width of the loaded map layout
+		 */
 		int mapWidth;
+
+		/*
+		 * The width of the loaded map layout
+		 */
 		int mapHeight;
 
+		/**
+		 * @return The position (index within mapLayout) of the tile at x,y
+		 */
 		int pos(int x, int y) const;
 	};
 
